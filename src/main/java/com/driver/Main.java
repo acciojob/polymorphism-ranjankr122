0fbd77a059dacc216cc50723ddf76c77c40ff061
Ranjan <rkram122@gmail.com>
2023-01-15 23:17:44 +0530

    Solution1.2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,21 +1,22 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
+        public int product(int x,int y){
+            return x*y;
+        }
+        public int product(int x,int y,int z){
+            return x*y*z;
+        }
+        public Double product(Double x,Double y){
+            return x*y;
+        }
+    }
     public static void main(String[] args) {
         Product p=new Product();
-        System.out.println(p.product(1, 5));
-        System.out.println(p.product(1,2,4));
-        System.out.println(p.product(20.0,1.2));
-    }
-}
-class Product{
-    public int product(int x,int y){
-        return x*x;
-    }
-    public int product(int x,int y,int z){
-        return x*y*z;
-    }
-    public double product(double x,double y){
-        return x*y;
+
+        p.product(1, 5);
+        p.product(1,2,4);
+        p.product(20.0,1.2);
     }
 }
--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -4,5 +4,20 @@ import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
-     
+     int x,y,z;
+     double x1,x2;
+     TestCases(int x,int y){
+         this.x=x;
+         this.y=y;
+     }
+     TestCases(int x,int y,int z){
+         this.x=x;
+         this.y=y;
+         this.z=z;
+
+     }
+     TestCases(Double x,Double y){
+         x1=x;
+         x2=y;
+     }
 }
